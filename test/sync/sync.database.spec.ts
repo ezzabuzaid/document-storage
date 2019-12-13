c8c09d2827fcd123802246313a1b174c9752ec4b
ezzabuzaid <ezzabuzaid@hotmail.com>
2019-12-13 19:16:35 +0200

    add jest, write test for sync database

---
+++ test/sync/sync.database.spec.ts
@@ -0,0 +1,24 @@
+import { Database, SyncStorage } from "../../src/index";
+
+describe('#Sync Database', () => {
+    const mockStorageFn = jest.fn<Partial<SyncStorage>, any>((() => ({
+        clear: jest.fn()
+    })));
+    const mockStorage = mockStorageFn() as SyncStorage;
+
+    const database = new Database(mockStorage);
+    it('should check if a collection is exist', () => {
+        expect(database.has('test')).toBeFalsy();
+    });
+
+    it('should add collection to the collections list', () => {
+        database.collection('test');
+        expect(database.has('test')).toBeTruthy();
+    });
+
+    it('should clear out all data inside the collections', () => {
+        database.collection('test');
+        database.clear();
+        expect(mockStorage.clear).toBeCalled();
+    });
+});
