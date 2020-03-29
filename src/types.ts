7a3ff5eb928c779e83c48253271384d4979c919c
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-29 18:23:59 +0300

    fix collection BASH=/bin/bash BASH_ARGC=() BASH_ARGV=() BASH_LINENO=() BASH_SOURCE=() BASH_VERSINFO=([0]="3" [1]="2" [2]="57" [3]="1" [4]="release" [5]="x86_64-apple-darwin19") BASH_VERSION='3.2.57(1)-release' COLORTERM=truecolor COLUMNS=231 COMMAND_MODE=unix2003 DIRSTACK=() EUID=502 GROUPS=() HISTFILE=/Users/ezz.abuzaid/.bash_history HISTFILESIZE=500 HISTSIZE=500 HOME=/Users/ezz.abuzaid HOSTNAME=Equitis-MacBook-Pro.local HOSTTYPE=x86_64 IFS=$' \t\n' LANG=en_US.UTF-8 LINES=22 LOGNAME=ezz.abuzaid LaunchInstanceID=946CF48D-D55B-4EB1-B7F6-88DB7D9A41EF MACHTYPE=x86_64-apple-darwin19 MAILCHECK=60 OPTERR=1 OPTIND=1 OSTYPE=darwin19 PATH='/Users/ezz.abuzaid/development/flutter/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/usr/local/share/dotnet:~/.dotnet/tools:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/opt/local/bin:/opt/local/sbin' PIPESTATUS=([0]="0") PPID=6281 PS1='\h:\W \u\$ ' PS2='> ' PS4='+ ' PWD=/Users/ezz.abuzaid/Desktop/projects/document-storage SECURITYSESSIONID=186a6 SHELL=/bin/zsh SHELLOPTS=braceexpand:emacs:hashall:histexpand:history:interactive-comments:monitor SHLVL=1 SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.tA5fp3BXoD/Listeners TERM=xterm-256color TERM_PROGRAM=vscode TERM_PROGRAM_VERSION=1.43.2 TMPDIR=/var/folders/q8/gvn_cyp53f1gybcwcg5yzh9h0000gp/T/ UID=502 USER=ezz.abuzaid XPC_FLAGS=0x0 XPC_SERVICE_NAME=com.apple.xpc.launchd.oneshot.0x10000001.Electron _=. __CF_USER_TEXT_ENCODING=0x1F6:0x0:0x0 types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export type Entity<T> = T & { readonly id: number };
 export interface SyncStorage {
     get<T>(name: string): Entity<T>[];
-    clear(name?: string): void;
-    set<T>(name: string, value: any): Entity<T>;
+    clear(name: string): void;
+    set<T>(name: string, value: any): void;
 }
 
 export interface AsyncStorage {
     get<T>(name: string): Promise<Entity<T>[]>;
-    clear(name?: string): Promise<void>;
-    set<T>(name: string, value: Entity<T>[]): Promise<Entity<T>>;
+    clear(name: string): Promise<void>;
+    set<T>(name: string, value: Entity<T>[]): Promise<void>;
 }
